f215e6524255c811575e7bb00fb0e5186827d16b
froliceric0914 <wzhao.eric@gmail.com>
2021-02-16 13:57:12 -0500

    use reduce for array_adv practice

--- functional_js_syntax/array_adv_map.js
+++ functional_js_syntax/array_adv_map.js
@@ -1,21 +1,40 @@
 // 1. TODO
-["1", "2", "3"].map(parseInt)
-
+;['1', '2', '3'].map(parseInt)
 
 // ----------------------------------------------------------
 // REAL LIFE EXERCISES
 // ----------------------------------------------------------
-// One common thing we use map for in real life is to reformat objects to have a shape that is 
-// better for our purposes. For instance, we only care about the value and first three characters 
+// One common thing we use map for in real life is to reformat objects to have a shape that is
+// better for our purposes. For instance, we only care about the value and first three characters
 // of the id for the races below. Use map to grab those values and create a new array with them.
 
 var index = [
-    {key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon'},
-    {key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari'},
-    {key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon'},
-    {key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa'},
+    { key: 1, sector: 10, t_score: 18, id: '1236n7e8', value: 'Klingon' },
+    { key: 4, sector: 145, t_score: 12, id: '293847hs8', value: 'Minbari' },
+    { key: 8, sector: 214, t_score: 5, id: '283hy8347', value: 'Cylon' },
+    { key: 3, sector: 8346, t_score: 10, id: 'n9837ks857', value: 'Jawa' },
 ]
 
+const modifiedIndex = index.map((e) => {
+    return {
+        id: e.id.substring(0, 3),
+        value: e.value,
+    }
+})
+
+const key_to_keep = ['id', 'value']
+
+const redux = (arr) =>
+    arr.map((e) =>
+        key_to_keep.reduce((acc, cur) => {
+            acc[cur] = e[cur].substring(0, 3)
+            return acc
+        }, {}),
+    )
+
+console.log('redux', redux(index))
+
+console.log('modifiedIndex', modifiedIndex)
 // Expected Output:
 // [
 //     {id: '123', value: 'Klingon'},
