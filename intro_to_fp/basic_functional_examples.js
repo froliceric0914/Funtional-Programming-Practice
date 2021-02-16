04ec4daf6730a5f5466fab8b8544eb9adc6a838c
froliceric0914 <wzhao.eric@gmail.com>
2021-02-15 21:00:41 -0500

    basic examples: write two pure functions

--- intro_to_fp/basic_functional_examples.js
+++ intro_to_fp/basic_functional_examples.js
@@ -5,14 +5,39 @@
 // Hint - this will help with time of day: new Date().getHours()
 // -----------------------------------------------------------------
 
+function printGreet() {
+    const time = new Date().getHours();
+    return time >= 0 && time <= 12 ? 'good morning' : 'good afternoon';
+}
 
-
+console.log(printGreet());
 
 // -----------------------------------------------------------------
 // Exercise 2
-// Directions: Write a pure function that takes in a number and  
-//       returns an array of items counting down from that number to 
+// Directions: Write a pure function that takes in a number and
+//       returns an array of items counting down from that number to
 //       zero.
 // -----------------------------------------------------------------
 
+// this one has side effect cuz it edit the countArr outside of the function
+// let countArr = [];
+// function countDown(num) {
+//     if (num > 0) {
+//         countArr.push(num);
+//         num--;
+//         console.log('countArr', num, countArr);
+//         return countDown(num);
+//     } else {
+//         return countArr;
+//     }
+// }
 
+function countDown(num) {
+    let countArr = [];
+    for (let i = num; i > 0; i--) {
+        countArr.push(i);
+    }
+    return countArr;
+}
+
+console.log(countDown(5));
