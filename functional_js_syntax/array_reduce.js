323874dade92c4696a82e9fb0c11ff090ddfe0e4
froliceric0914 <wzhao.eric@gmail.com>
2021-02-16 18:04:09 -0500

    finish the reduce practice

--- functional_js_syntax/array_reduce.js
+++ functional_js_syntax/array_reduce.js
@@ -1,6 +1,14 @@
 // 1. Take this disjointed sentence and turn it into a single string
-const text = ['The ships', 'hung in the sky,', 'much the way', 'that bricks don`t']
+const text = [
+    'The ships',
+    'hung in the sky,',
+    'much the way',
+    'that bricks don`t',
+]
 
+const joinedText = text.reduce((sentence, element) => sentence + `${element} `)
+
+console.log('joinedText', joinedText)
 // Your Code Here
 
 // expected output: "The ships hung in the sky, much the way that bricks don't"
@@ -11,23 +19,35 @@ const text = ['The ships', 'hung in the sky,', 'much the way', 'that bricks don`
 const scores = [
     {
         team: 'A',
-        score: 20
+        score: 20,
     },
     {
         team: 'B',
-        score: 17
+        score: 17,
     },
     {
         team: 'C',
-        score: 23
+        score: 23,
     },
     {
         team: 'D',
-        score: 13
-    }
+        score: 13,
+    },
 ]
 
 // Your Code Here
+
+const highestScore = function () {
+    let finalTeam = scores.reduce((higherScore, currentScore) => {
+        return higherScore.score > currentScore.score
+            ? higherScore
+            : currentScore
+    })
+    console.log('finalTeam', finalTeam)
+    return finalTeam.team
+}
+
+console.log('highestScore', highestScore())
 
 // expected output: "C"
 
@@ -54,9 +74,16 @@ const ships = [
     {
         name: 'Tie Fighters',
         speed: '4100G',
-    }
+    },
 ]
 
 // Your Code Here
+const highSpeed = ships.reduce((faster, current) => {
+    let fasterSpeed = parseInt(faster.speed.slice(0, -1))
+    let currentSpeed = parseInt(current.speed.slice(0, -1))
+    return (faster = fasterSpeed > currentSpeed ? faster : current)
+})
+
+console.log('highSpeed', highSpeed.name)
 
 // Expected output: Tie Fighters
