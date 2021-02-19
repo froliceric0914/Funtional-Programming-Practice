8e156561a3dd74958cd9808f84697a5925c27d63
froliceric0914 <wzhao.eric@gmail.com>
2021-02-19 14:40:24 -0500

    Higher order function practice 2 completed

--- fp_in_js/hof_2.js
+++ fp_in_js/hof_2.js
@@ -1,17 +1,37 @@
 weaponsWithNoises = [
-  {name: 'Phaser', noise: 'bssszzsssss', universe: 'Star Trek'},
-	{name: 'Blaster', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Sonic Screwdriver', noise: 'Pew Pew', universe: 'Dr. Who'},
-	{name: 'Lightsaber', noise: 'Pew Pew', universe: 'Star Wars'},
-	{name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black'}
+    { name: 'Phaser', noise: 'bssszzsssss', universe: 'Star Trek' },
+    { name: 'Blaster', noise: 'Pew Pew', universe: 'Star Wars' },
+    { name: 'Sonic Screwdriver', noise: 'Pew Pew', universe: 'Dr. Who' },
+    { name: 'Lightsaber', noise: 'Pew Pew', universe: 'Star Wars' },
+    { name: 'Noisy Cricket', noise: 'Pew Pew', universe: 'Men in Black' },
 ]
 
+//pass a unvierse and then console.log the noise
 function weaponsFromUniverse(universe) {
-	// ...Your code here!
+    const usableWeapon = weaponsWithNoises.filter(
+        (weapon) => weapon.universe === universe,
+    )
+
+    const useWeapon = (weaponName) => {
+        const weapon = usableWeapon.find((w) => w.name === weaponName)
+
+        if (weapon) {
+            console.log(weapon.noise)
+        } else {
+            console.log(
+                `${weaponName} is not a part of the ${universe} universe `,
+            )
+        }
+    }
+
+    return useWeapon
 }
 
 // USAGE
 const useStarWarsWeapon = weaponsFromUniverse('Star Wars')
 
+weaponsFromUniverse('Star Wars')('Lightsaber')
+
 useStarWarsWeapon('Blaster') // console logs 'used Blaster: Pew Pew'
+
 useStarWarsWeapon('Noisy Cricket') // console logs 'Noisy Cricket is not a part of the Star Wars universe'
